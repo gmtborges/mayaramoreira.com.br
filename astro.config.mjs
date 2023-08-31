9664400f94ec9d22e75d60dd1e43d70b8197fed1
G M T Borges <contact@gmtborges.com>
2023-08-31 09:17:18 -0300

    update astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
 import compress from 'astro-compress';
 import icon from 'astro-icon';
-import tasks from "./src/utils/tasks";
+import tasks from './src/utils/tasks';
 
 import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
 
@@ -76,10 +76,6 @@ export default defineConfig({
 
   markdown: {
     remarkPlugins: [readingTimeRemarkPlugin],
-  },
-
-  experimental:{
-    assets: true
   },
 
   vite: {
